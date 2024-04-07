32330ce35b3a8b69e2903a27ed7d475c6339ed3a
Amanda <hamockingjay@gmail.com>
2024-04-06 20:41:10 -0400

    Lowercase

--- PingIt/Windows/ConfigWindow.cs
+++ PingIt/Windows/ConfigWindow.cs
@@ -7,7 +7,7 @@ namespace PingIt.Windows;
 
 public class ConfigWindow : Window, IDisposable
 {
-    private Configuration Configuration;
+    private Configuration configuration;
 
     public ConfigWindow(Plugin plugin) : base(
         "A Wonderful Configuration Window",
@@ -17,7 +17,7 @@ public class ConfigWindow : Window, IDisposable
         this.Size = new Vector2(232, 75);
         this.SizeCondition = ImGuiCond.Always;
 
-        this.Configuration = plugin.Configuration;
+        this.configuration = plugin.Configuration;
     }
 
     public void Dispose() { }
@@ -25,12 +25,12 @@ public class ConfigWindow : Window, IDisposable
     public override void Draw()
     {
         // can't ref a property, so use a local copy
-        var configValue = this.Configuration.SomePropertyToBeSavedAndWithADefault;
+        var configValue = this.configuration.SomePropertyToBeSavedAndWithADefault;
         if (ImGui.Checkbox("Random Config Bool", ref configValue))
         {
-            this.Configuration.SomePropertyToBeSavedAndWithADefault = configValue;
+            this.configuration.SomePropertyToBeSavedAndWithADefault = configValue;
             // can save immediately on change, if you don't want to provide a "Save and Close" button
-            this.Configuration.Save();
+            this.configuration.Save();
         }
     }
 }
